b1695c1900ceb6bd610bb5c4120e9458c23d43e2
Joao Cavadas Ribeiro <jcavadas@c1r7s1.42porto.com>
2023-09-12 05:23:35 +0100

    Correction in C_04 ex01

--- C_04/ex01/ft_putstr.c
+++ C_04/ex01/ft_putstr.c
@@ -24,13 +24,11 @@ void	ft_putstr(char *str)
 	}
 }
 /*
-#include <stdio.h>
-
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		printf("Input nao aceite!");
+		ft_putstr("Input nao aceite!");
 		return (0);
 	}
 	ft_putstr(argv[1]);
